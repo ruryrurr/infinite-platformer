8a4c32e04d44b2fec2f1d33f47ad3f56f4ca47c8
Atharva Nandan <109989084+ruryrurr@users.noreply.github.com>
2022-07-28 21:14:11 +0000

    getting the bricks to appear

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
 import sys
+import random
 from pygame.locals import*
+from platforms import Platforms
+
+sprite_list = pygame.sprite.Group()
+platform = pygame.sprite.Group()
 
 pygame.init()
 screen_info = pygame.display.Info()
 
 size = (width, height) = (int(screen_info.current_w),
                           int(screen_info.current_h))
-
 screen = pygame.display.set_mode(size)
 
 clock = pygame.time.Clock()
 
 screen.fill((30, 0, 0))
 
-sprite_list = pygame.sprite.Group()
-platform = pygame.sprite.Group()
+
+
+
+def init():
+  for i in range(height//100):
+    for j in range(width//410):
+      plat = Platforms((random.randint(5, (width - 50) // 10)* 10, 120 * i), 'images/grassHalf.png', 70, 40)
+      platform.add(plat)
+
 
 
 def main():
+  init()
   while True:
     for event in pygame.event.get():
       if event.type == QUIT:
