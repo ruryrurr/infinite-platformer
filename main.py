06515c9c5eae99959e3e4fdfcd8c5487dc65b8eb
Atharva Nandan <109989084+ruryrurr@users.noreply.github.com>
2022-07-28 23:02:53 +0000

    we can see guy now fixed some bugs

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
 import pygame
 import sys
 import random
-from pygame.locals import*
+from pygame.locals import *
 from platforms import Platforms
+from player import Player
+
+
 
 sprite_list = pygame.sprite.Group()
 platform = pygame.sprite.Group()
+player = ''
+
+
 
 pygame.init()
 screen_info = pygame.display.Info()
-
 size = (width, height) = (int(screen_info.current_w),
                           int(screen_info.current_h))
 screen = pygame.display.set_mode(size)
-
 clock = pygame.time.Clock()
-
 screen.fill((30, 0, 0))
 
 
-def init():
+
+def get_player_action():
+  p1_action = {}
+  p1_action['p1_jump'] = pygame.image.load("images/p1_jump.png").convert()
+  p1_action['p1_jump'].set_colorkey((0,0,0))
+  p1_action['p1_hurt'] = pygame.image.load("images/p1_hurt.png").convert()
+  p1_action['p1_hurt'].set_colorkey((0,0,0))
+  return p1_action
+  
+
+
+def init(p1_action):
+  global player 
   for i in range(height//100):
     for j in range(width//410):
       plat = Platforms((random.randint(5, (width - 50) // 10)* 10, 120 * i), 'images/grassHalf.png', 70, 40)
       platform.add(plat)
+  player = Player((platform.sprites()[-1].rect.centerx,platform.sprites()[-1].rect.centery-300), p1_action) 
+  sprite_list.add(player)
 
-
-def init():
-  for i in range(height//100):
-    for j in range(width//410):
-      plat = Platforms((random.randint(5, (width - 50) // 10)* 10, 120 * i), 'images/grassHalf.png', 70, 40)
-      platform.add(plat)
-
-
+  
 
 def main():
-  init()
+  global player
+  p1_action  = get_player_action()
+  init(p1_action)
   while True:
     for event in pygame.event.get():
       if event.type == QUIT:
@@ -46,7 +58,6 @@ def main():
         if event.key==pygame.K_ESCAPE:
           pygame.display.set_mode(size)
           
-        
 
           
     screen.fill((30, 0, 30))
@@ -57,7 +68,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+  main()
 
 
 
